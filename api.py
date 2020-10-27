5143bb43c187b4b62f65fdc6704ddc9d7e35de60
yujunkuo <johnboy880313@gmail.com>
2020-10-27 22:31:37 +0800

    change api return

--- api.py
+++ api.py
@@ -18,7 +18,7 @@ with open("./config/server.yml", 'r') as stream:
         print(exc)
 
 # read database
-with open("./confiq/database.yml", 'r') as stream:
+with open("./config/database.yml", 'r') as stream:
     try:
         mysql_config = yaml.safe_load(stream)
     except yaml.YAMLError as exc:
@@ -36,14 +36,16 @@ mysql = MySQL(app)
 # Insert register information
 
 
-def insert_registeration(email: str, gender: str, department: str, password: str, mysql=mysql):
+def insert_registeration(uid:str, email: str, gender: str, department: str, password: str, mysql=mysql):
     cur = mysql.connection.cursor()
-    sql_command = "INSERT INTO user_info (email, gender, department, password) VALUES(%s,%s,%s,%s);"
+    sql_command = "INSERT INTO library_db.user_info (uid, email, gender, department, password) VALUES (%s,%s,%s,%s,%s);"
     try:
-        cur.execute(sql_command, (email, gender, department, password))
-        fetch_data = cur.fetchall()
-        cur.close()
-        return fetch_data
+        cur.execute(sql_command, (uid, email, gender, department, password))
+        mysql.connection.commit()
+        
+        # fetch_data = cur.fetchall()
+        # cur.close()
+        # return fetch_data
     except Exception as e:
         raise(e)
 
@@ -53,15 +55,18 @@ def insert_registeration(email: str, gender: str, department: str, password: str
 @app.route('/api/v1/register/', methods=['POST'])
 def register_api():
     data = request.get_json()
+    uid = data["uid"]
     email = data["email"]
     gender = data["gender"]
     department = data["department"]
     password = data["password"]
     start_time = time.time()
-    return_dict = insert_registeration(email, gender, department, password)
+    insert_registeration(uid, email, gender, department, password)
     end_time = time.time()
     handle_time = round(end_time - start_time, 2)
+    return_dict = {}
     return_dict["handle_time"] = handle_time
+    
     return jsonify(return_dict)
 
 
@@ -95,12 +100,13 @@ def get_userinfo(email: str, mysql=mysql):
     cur = mysql.connection.cursor()
     sql_command = "SELECT * FROM user_info WHERE email = %s;"
     cur.execute(sql_command, (email, ))
-    fetch_data = cur.fetchall()
+    #fetch_data = cur.fetchall()
+    columns = [col[0] for col in cur.description]
+    fetch_data = {col: row for col, row in zip(columns, cur.fetchall()[0])}
     cur.close()
-    return fetch_data
+    return  fetch_data
 
 # User Information API (Include recent loan data)
-
 
 @app.route('/api/v1/userinfo/', methods=['POST'])
 def userinfo_api():
@@ -166,7 +172,7 @@ def iitem_api():
 
 
 def main():
-    app.run(debug=True, host=server_config["ip"])
+    app.run(debug=True, host=server_config["ip"], port=5001)
 
 
 if __name__ == "__main__":
